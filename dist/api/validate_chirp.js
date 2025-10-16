4e72fed48b0ac683ba9b88a3e8de0d2cdc261807
RealFoz <scottpool@hotmail.com>
2025-10-16 22:35:31 +0100

    updated validate_chirp endpoint with profanity filter

--- dist/api/validate_chirp.js
+++ dist/api/validate_chirp.js
@@ -8,7 +8,11 @@ export function validateChirpHandler(req, res) {
             res.status(400).json({ error: "Chirp is too long" }); // sets the res status to 400 with the err msg as an object if over 140chars
             return;
         }
-        res.status(200).json({ valid: true }); //stringifys the JSON and puts it as the response with `ok` code 200
+        // "swear" filter layer
+        const words = req.body.body.split(" "); // forces the array to be only of strings so TS is happy
+        const censored = words.map(word => profanityFinder(word)); //maps each word to the profanity helper function      
+        const cleanedBody = censored.join(" ");
+        res.status(200).json({ cleanedBody }); //stringifys the JSON and puts it as the response with `ok` code 200
         return;
     }
     catch {
@@ -16,3 +20,15 @@ export function validateChirpHandler(req, res) {
         return;
     }
 }
+function profanityFinder(word) {
+    if (ProfanitySet.has(word.toLowerCase())) { // just checks if each word passed in is made lowercase and checks if its in the set, if it is it gets **** overwritten
+        word = "****";
+        return word;
+    }
+    return word;
+}
+const ProfanitySet = new Set([
+    `kerfuffle`,
+    `sharbert`,
+    `fornax`
+]);
--- src/api/validate_chirp.ts
+++ src/api/validate_chirp.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 
-type responseData = {valid: true} | {error: string} // response is either valid or an error so we can set the base value to true.
 
-export  function validateChirpHandler(req: Request, res: Response<responseData>) { // refactored to use the express json middleware so all responses use the .json() method. can only have response be one of our set values in the type union above
+
+export  function validateChirpHandler(req: Request, res: Response) { // refactored to use the express json middleware so all responses use the .json() method.
         try {
             if (!req.body.body || typeof req.body.body !== "string") {
                 res.status(400).json({error: "Something went wrong"}) // checks type is a string after its parsed
@@ -12,10 +12,28 @@ export  function validateChirpHandler(req: Request, res: Response<responseData>)
               res.status(400).json({ error: "Chirp is too long" }); // sets the res status to 400 with the err msg as an object if over 140chars
               return;
             }
-            res.status(200).json({ valid: true }); //stringifys the JSON and puts it as the response with `ok` code 200
+            // "swear" filter layer
+            const words: string[] = req.body.body.split(" ") // forces the array to be only of strings so TS is happy
+            const censored = words.map(word => profanityFinder(word)) //maps each word to the profanity helper function      
+            const cleanedBody = censored.join(" ") 
+            res.status(200).json({cleanedBody}); //stringifys the JSON and puts it as the response with `ok` code 200
             return;
         } catch {
              res.status(400).json({error: "Something went wrong"}) // fail safe to catch uneexpected stuff and handle it gracefully
              return;
         }
 }
+
+function profanityFinder(word: string): string {
+  if (ProfanitySet.has(word.toLowerCase())) { // just checks if each word passed in is made lowercase and checks if its in the set, if it is it gets **** overwritten
+    word = "****"
+    return word;
+  }
+  return word;
+}
+
+const ProfanitySet = new Set([ // set so its scalable and i think its better on the big O stuff
+  `kerfuffle`,
+  `sharbert`,
+  `fornax`
+])
